69fe5b9908d29872799ed7aeda1cc7b35b410984
SobirjonDEV <sobirjonzafarovich09@gmail.com>
2022-05-04 17:27:16 +0500

    edit to 404 in 5555

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -8,7 +8,7 @@
         src="	https://xtrm.foxthemes.me/wp-content/uploads/2018/09/white-copy-9-1.png"
         alt="">
         <span >
-          404
+          5555
         </span>
       </div>
 
